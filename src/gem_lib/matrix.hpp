429035c3be4c9fc3bfa9b632dacfb72c8af3d3fd
Šimon Brandner <simon.bra.ag@gmail.com>
2024-12-12 15:17:00 +0100

    Move everything into the declaration
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/gem_lib/matrix.hpp
+++ src/gem_lib/matrix.hpp
@@ -19,7 +19,9 @@ template <typename T> class Matrix {
 	size_t number_of_columns;
 	std::vector<T> data;
 
-	T &at(size_t row, size_t column);
+	T &at(size_t row, size_t column) {
+		return this->data[row * this->number_of_columns + column];
+	}
 
 	public:
 	// This is necessary due to the stupidity of C++ which instead of requiring
@@ -28,77 +30,116 @@ template <typename T> class Matrix {
 	// initializer list which we do not want to do. Rust would work much better
 	// here.
 	Matrix() = default;
+
 	Matrix(
 		std::vector<T> data, size_t number_of_rows, size_t number_of_columns
-	);
+	) {
+		if (data.size() != (number_of_rows * number_of_columns)) {
+			throw std::runtime_error("The supplied data has the wrong size");
+		}
 
-	const size_t get_number_of_rows() const;
-	const size_t get_number_of_columns() const;
-	const T &at(size_t row, size_t column) const;
+		// We could use a member initializer list here but I find this a little
+		// better since it's a bit more explicit
+		this->data = data;
+		this->number_of_rows = number_of_rows;
+		this->number_of_columns = number_of_columns;
+	}
 
-	const Matrix<T> right_join(const Matrix<T> &rhs) const;
-	const Matrix<T> operator*(const Matrix<T> &rhs) const;
-	const Matrix<T> operator-(const Matrix<T> &rhs) const;
+	const size_t get_number_of_rows() const { return this->number_of_rows; }
+
+	const size_t get_number_of_columns() const {
+		return this->number_of_columns;
+	}
+
+	const T &at(size_t row, size_t column) const {
+		return this->data[row * this->number_of_columns + column];
+	}
+
+	const Matrix<T> right_join(const Matrix<T> &rhs) const {
+		if (this->number_of_rows != rhs.get_number_of_rows()) {
+			throw std::runtime_error("The number of rows does not match!");
+		}
+
+		std::vector<T> new_data;
+		new_data.reserve(
+			this->number_of_rows *
+			(this->number_of_columns + rhs.number_of_columns)
+		);
+
+		for (size_t row = 0; row < this->number_of_rows; ++row) {
+			for (size_t column = 0; column < this->number_of_columns;
+				 ++column) {
+				new_data.push_back(this->at(row, column));
+			}
+			for (size_t column = 0; column < rhs.get_number_of_columns();
+				 ++column) {
+				new_data.push_back(rhs.at(row, column));
+			}
+		}
+
+		return Matrix<T>(
+			new_data,
+			this->number_of_rows,
+			this->number_of_columns + rhs.get_number_of_columns()
+		);
+	}
+
+	const Matrix<T> operator*(const Matrix<T> &rhs) const {
+		// Matrix A is R^t -> R^r and Matrix B is R^c -> R^p but we cannot
+		// compose R^c -> R^p and R^t -> R^r since p != t
+		if (this->number_of_columns != rhs.number_of_rows) {
+			std::stringstream error_message;
+			error_message << "Cannot compose R^" << this->number_of_columns
+						  << " -> R^" << this->number_of_rows << " with R^"
+						  << rhs.number_of_columns << "R^" << rhs.number_of_rows
+						  << "!";
+			throw std::runtime_error(error_message.str());
+		}
+		// Matrix A is R^t -> R^r and Matrix B is R^c -> R^t. So when we compose
+		// R^c -> R^t and R^t -> R^r, we get R^c -> R^r
+		size_t result_number_of_rows = this->number_of_rows;
+		size_t result_number_of_columns = rhs.number_of_columns;
+		std::vector<T> result_data(
+			result_number_of_rows * result_number_of_columns, 0
+		);
+
+		for (size_t row = 0; row < result_number_of_rows; ++row) {
+			for (size_t column = 0; column < result_number_of_columns;
+				 ++column) {
+				result_data[row * result_number_of_columns + column] = 0;
+				for (size_t i = 0; i < this->number_of_columns; ++i) {
+					result_data[row * result_number_of_columns + column] +=
+						this->at(row, i) * rhs.at(i, column);
+				}
+			}
+		}
+
+		return Matrix<T>(
+			result_data, result_number_of_rows, result_number_of_columns
+		);
+	}
+
+	const Matrix<T> operator-(const Matrix<T> &rhs) const {
+		if (this->number_of_rows != rhs.number_of_rows ||
+			this->number_of_columns != rhs.number_of_columns) {
+			throw std::runtime_error(
+				"Cannot subtract matrices of different sizes!"
+			);
+		}
+
+		std::vector<T> result_data(
+			this->number_of_rows * this->number_of_columns, 0
+		);
+
+		for (size_t i = 0; i < this->data.size(); ++i) {
+			result_data[i] = this->data[i] - rhs.data[i];
+		}
+
+		return Matrix<T>(
+			result_data, this->number_of_rows, this->number_of_columns
+		);
+	}
 };
-
-template <typename T>
-Matrix<T>::Matrix(
-	std::vector<T> data, size_t number_of_rows, size_t number_of_columns
-) {
-	if (data.size() != (number_of_rows * number_of_columns)) {
-		throw std::runtime_error("The supplied data has the wrong size");
-	}
-
-	// We could use a member initializer list here but I find this a little
-	// better since it's a bit more explicit
-	this->data = data;
-	this->number_of_rows = number_of_rows;
-	this->number_of_columns = number_of_columns;
-}
-
-template <typename T> const size_t Matrix<T>::get_number_of_columns() const {
-	return this->number_of_columns;
-}
-
-template <typename T> const size_t Matrix<T>::get_number_of_rows() const {
-	return this->number_of_rows;
-}
-
-template <typename T> const T &Matrix<T>::at(size_t row, size_t column) const {
-	return this->data[row * this->number_of_columns + column];
-}
-
-template <typename T> T &Matrix<T>::at(size_t row, size_t column) {
-	return this->data[row * this->number_of_columns + column];
-}
-
-template <typename T>
-Matrix<T> const Matrix<T>::right_join(const Matrix<T> &rhs) const {
-	if (this->number_of_rows != rhs.get_number_of_rows()) {
-		throw std::runtime_error("The number of rows does not match!");
-	}
-
-	std::vector<T> new_data;
-	new_data.reserve(
-		this->number_of_rows * (this->number_of_columns + rhs.number_of_columns)
-	);
-
-	for (size_t row = 0; row < this->number_of_rows; ++row) {
-		for (size_t column = 0; column < this->number_of_columns; ++column) {
-			new_data.push_back(this->at(row, column));
-		}
-		for (size_t column = 0; column < rhs.get_number_of_columns();
-			 ++column) {
-			new_data.push_back(rhs.at(row, column));
-		}
-	}
-
-	return Matrix<T>(
-		new_data,
-		this->number_of_rows,
-		this->number_of_columns + rhs.get_number_of_columns()
-	);
-}
 
 template <typename T> const double abs(const Matrix<T> &matrix) {
 	double sum = 0;
@@ -109,62 +150,6 @@ template <typename T> const double abs(const Matrix<T> &matrix) {
 		}
 	}
 	return sqrt(sum);
-}
-
-template <typename T>
-Matrix<T> const Matrix<T>::operator*(const Matrix<T> &rhs) const {
-	// Matrix A is R^t -> R^r and Matrix B is R^c -> R^p but we cannot compose
-	// R^c -> R^p and R^t -> R^r since p != t
-	if (this->number_of_columns != rhs.number_of_rows) {
-		std::stringstream error_message;
-		error_message << "Cannot compose R^" << this->number_of_columns
-					  << " -> R^" << this->number_of_rows << " with R^"
-					  << rhs.number_of_columns << "R^" << rhs.number_of_rows
-					  << "!";
-		throw std::runtime_error(error_message.str());
-	}
-	// Matrix A is R^t -> R^r and Matrix B is R^c -> R^t. So when we compose
-	// R^c -> R^t and R^t -> R^r, we get R^c -> R^r
-	size_t result_number_of_rows = this->number_of_rows;
-	size_t result_number_of_columns = rhs.number_of_columns;
-	std::vector<T> result_data(
-		result_number_of_rows * result_number_of_columns, 0
-	);
-
-	for (size_t row = 0; row < result_number_of_rows; ++row) {
-		for (size_t column = 0; column < result_number_of_columns; ++column) {
-			result_data[row * result_number_of_columns + column] = 0;
-			for (size_t i = 0; i < this->number_of_columns; ++i) {
-				result_data[row * result_number_of_columns + column] +=
-					this->at(row, i) * rhs.at(i, column);
-			}
-		}
-	}
-
-	return Matrix<T>(
-		result_data, result_number_of_rows, result_number_of_columns
-	);
-}
-
-template <typename T>
-Matrix<T> const Matrix<T>::operator-(const Matrix<T> &rhs) const {
-	if (this->number_of_rows != rhs.number_of_rows ||
-		this->number_of_columns != rhs.number_of_columns) {
-		throw std::runtime_error("Cannot subtract matrices of different sizes!"
-		);
-	}
-
-	std::vector<T> result_data(
-		this->number_of_rows * this->number_of_columns, 0
-	);
-
-	for (size_t i = 0; i < this->data.size(); ++i) {
-		result_data[i] = this->data[i] - rhs.data[i];
-	}
-
-	return Matrix<T>(
-		result_data, this->number_of_rows, this->number_of_columns
-	);
 }
 
 template <typename T>
