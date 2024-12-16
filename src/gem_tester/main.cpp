d08c954ad81037600ca2ac536e5c019e6359f748
Šimon Brandner <simon.bra.ag@gmail.com>
2024-12-16 08:28:38 +0100

    Start work on argument parsing
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/gem_tester/main.cpp
+++ src/gem_tester/main.cpp
@@ -1,62 +1,176 @@
 #include "../gem_lib/matrix.hpp"
 #include "../gem_lib/system_of_equations.hpp"
 
+#include <chrono>
+#include <functional>
 #include <iostream>
+#include <stdexcept>
 
-#define MIN 100
-#define MAX -100
-#define EPSILON 0.01
+/*
+ * The code in here could certainly be improved but since argument parsing was
+ * not the focus of the semestral work, we leave it as is. An argument parsing
+ * library like Rust's clap would do wonders here. (But at that point it is
+ * somewhat tempting to use Rust for the whole thing)
+ */
+
 #define FLOAT_TYPE double
 
-void test_solve(Matrix<FLOAT_TYPE> map, Matrix<FLOAT_TYPE> expected_solution) {
-	auto expected_right_side = map * expected_solution;
+constexpr double MIN = 100;
+constexpr double MAX = -100;
 
-	auto solution = solve_system_of_equations(map, expected_right_side);
+enum class Command { Generate, Solve, Complexity, Determinant };
+enum class ComplexityTask { SystemOfEquations, MatrixEquation, Determinant };
+enum class SystemMethod { Parallel, Sequential };
 
-	auto right_side = map * solution;
-	auto difference = expected_right_side - right_side;
-	auto distance = abs(difference);
+Command string_to_command(const std::string &string_command) {
+	static const std::unordered_map<std::string, Command> command_map = {
+		{"generate", Command::Generate},
+		{"solve", Command::Solve},
+		{"determinant", Command::Determinant},
+		{"complexity", Command::Complexity}
+	};
 
-	if (distance < EPSILON) {
-		std::cout << "Solved correctly" << std::endl;
-	} else {
-		std::cout << "Failed to solve correctly with distance: " << distance
-				  << std::endl;
+	auto it = command_map.find(string_command);
+	if (it != command_map.end()) {
+		return it->second;
 	}
+	throw std::runtime_error("Unknown command: " + string_command);
 }
 
-void test_system(size_t size) {
-	test_solve(
+ComplexityTask string_to_complexity_task(const std::string &string_task) {
+	static const std::unordered_map<std::string, ComplexityTask> task_map = {
+		{"determinant", ComplexityTask::Determinant},
+		{"system", ComplexityTask::SystemOfEquations},
+		{"equation", ComplexityTask::MatrixEquation}
+	};
+
+	auto it = task_map.find(string_task);
+	if (it != task_map.end()) {
+		return it->second;
+	}
+	throw std::runtime_error(
+		"Unknown task for complexity measurement: " + string_task
+	);
+}
+
+bool string_to_parallel(const std::string &string_method) {
+	static const std::unordered_map<std::string, bool> method_map = {
+		{"parallel", true}, {"sequential", false}
+	};
+
+	auto it = method_map.find(string_method);
+	if (it != method_map.end()) {
+		return it->second;
+	}
+	throw std::runtime_error("Unknown command: " + string_method);
+}
+
+DeterminantMethod string_to_determinant_method(const std::string &string_method
+) {
+	static const std::unordered_map<std::string, DeterminantMethod> method_map =
+		{{"parallel-elimination", DeterminantMethod::ParallelElimination},
+		 {"elimination", DeterminantMethod::Elimination},
+		 {"definition", DeterminantMethod::Definition}};
+
+	auto it = method_map.find(string_method);
+	if (it != method_map.end()) {
+		return it->second;
+	}
+	throw std::runtime_error(
+		"Unknown method for solving systems: " + string_method
+	);
+}
+
+void solve_random_system(size_t size, bool parallel) {
+	solve_system_of_equations(
 		Matrix<FLOAT_TYPE>::random(size, MIN, MAX),
 		Matrix<FLOAT_TYPE>::random(size, 1, MIN, MAX)
 	);
 }
 
-void test_matrix_equation(size_t size) {
-	std::cout << "Random big test matrix equation" << std::endl;
-	test_solve(
+void solve_random_matrix_equation(size_t size, bool parallel) {
+	solve_system_of_equations(
 		Matrix<FLOAT_TYPE>::random(size, MIN, MAX),
 		Matrix<FLOAT_TYPE>::random(size, MIN, MAX)
 	);
 }
 
-void test_determinant(size_t size) {
+void compute_random_determinant(size_t size, DeterminantMethod method) {
 	auto matrix = Matrix<FLOAT_TYPE>::random(size, MIN, MAX);
-	std::cout << "Determinant by definition: "
-			  << matrix.get_determinant(DeterminantMethod::Definition)
-			  << std::endl;
-	std::cout << "Determinant by elimination: "
-			  << matrix.get_determinant(DeterminantMethod::Elimination)
-			  << std::endl;
-	std::cout << "Determinant by parallel elimination: "
-			  << matrix.get_determinant(DeterminantMethod::ParallelElimination)
-			  << std::endl;
+	matrix.get_determinant(method);
 }
 
-int main(void) {
-	test_system(1000);
-	/*test_random_big_matrix_equation(1000);*/
-	/*test_determinant(5);*/
+void handle_complexity_task(
+	ComplexityTask task,
+	const std::string &method,
+	const size_t start_size,
+	const size_t step_size,
+	const size_t stop_size
+) {
+	std::function<void(size_t)> task_function;
+	switch (task) {
+	case ComplexityTask::SystemOfEquations: {
+		task_function = [method](size_t i) {
+			solve_random_system(i, string_to_parallel(method));
+		};
+		break;
+	}
+	case ComplexityTask::MatrixEquation: {
+		task_function = [method](size_t i) {
+			solve_random_matrix_equation(i, string_to_parallel(method));
+		};
+		break;
+	}
+	case ComplexityTask::Determinant: {
+		task_function = [method](size_t i) {
+			compute_random_determinant(i, string_to_determinant_method(method));
+		};
+		break;
+	}
+	}
+
+	for (size_t i = start_size; i < stop_size; i += step_size) {
+		auto start = std::chrono::high_resolution_clock::now();
+		task_function(i);
+		std::chrono::duration<double> elapsed =
+			std::chrono::high_resolution_clock::now() - start;
+		std::cout << "(" << i << ", " << elapsed.count() << ")"
+				  << "," << std::endl;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		throw std::runtime_error("No args provided!");
+	}
+
+	Command command = string_to_command(argv[1]);
+
+	switch (command) {
+	case Command::Generate: {
+		break;
+	}
+	case Command::Solve: {
+		break;
+	}
+	case Command::Determinant: {
+		break;
+	}
+	case Command::Complexity: {
+		if (argc < 7) {
+			throw std::runtime_error("Not enough arguments!");
+		}
+
+		ComplexityTask task = string_to_complexity_task(argv[2]);
+		std::string method = argv[3];
+		size_t start_size = std::stoi(argv[4]);
+		size_t step_size = std::stoi(argv[5]);
+		size_t stop_size = std::stoi(argv[6]);
+
+		handle_complexity_task(task, method, start_size, step_size, stop_size);
+		break;
+	}
+	}
 
 	return 0;
 }
